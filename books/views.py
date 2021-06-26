3e8cece4dbe04616fde2b0d10966cf2652651fd4
Darpan Pednekar <pednekardarpan@gmail.com>
2021-06-26 15:50:09 +0530

    json data setup

--- books/views.py
+++ books/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+
+booksData=open(r'C:\Users\Darpan\Desktop\Projects\Django 2021\Django-Course\bookstore-Project\books.json').read()
+
+data=json.loads(booksData)
 
 def index(request):
     #render function searches for templates folder in the app
-    context={'name':'Darpan'}
+    context={'books':data}
     return render(request,'books/index.html',context)
