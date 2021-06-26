079dc958af6367b91b4f66b6cd8ff6c340fcfa48
Darpan Pednekar <pednekardarpan@gmail.com>
2021-06-26 19:45:56 +0530

    admin super user

--- books/models.py
+++ books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import json
 
 # Create your models here.
+#darpan pass
+class Book(models.Model):
+    title=models.CharField(max_length=256)
+    isbn=models.CharField(max_length=256)
+    pageCount=models.IntegerField(default=0)
+    publishedDate=models.DateTimeField(auto_now_add=True)
+    thumbnailUrl=models.CharField(max_length=256,null=True)
+    shortDescription=models.CharField(max_length=512,null=True,blank=True)
+    longDescription=models.TextField(null=True)
+    status=models.CharField(max_length=256)
+    authors=models.CharField(max_length=256)
+    categories=models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.title
--- books/views.py
+++ books/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
+from .models import Book
+#booksData=open(r'C:\Users\Darpan\Desktop\Projects\Django 2021\Django-Course\bookstore-Project\books.json').read()
 
-booksData=open(r'C:\Users\Darpan\Desktop\Projects\Django 2021\Django-Course\bookstore-Project\books.json').read()
-
-data=json.loads(booksData)
+#data=json.loads(booksData)
 
 def index(request):
     #render function searches for templates folder in the app
-    context={'books':data}
+    dbdata=Book.objects.all()
+    context={'books':dbdata}
     return render(request,'books/index.html',context)
 
 def book_details(request,id):
-    singleBook=[]
-    for book in data:
-        if book['id']==id:
-            singleBook=book
+    singleBook=Book.objects.get(pk=id)
     context={'book':singleBook}
     return render(request,'books/book_details.html',context)
