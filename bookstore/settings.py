b590cc5a6e4a18f8335964fb1d62fbf93db57146
darpan45 <44457866+darpan45@users.noreply.github.com>
2021-07-03 18:06:36 +0530

    Update settings.py
    secret key removed

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =os.environ.get('DJANGO_SECRET_KEY','s_ud7uf7^$+52_2%uq(6k-+#2mtc2p=%udo2!ho)@nug67el08') 
+SECRET_KEY =os.environ.get('DJANGO_SECRET_KEY','') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG','') =='True'
