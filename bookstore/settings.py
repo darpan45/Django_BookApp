d618709175e6bd4f183c6328146d7de3c9a89354
Darpan Pednekar <pednekardarpan@gmail.com>
2021-07-03 17:18:25 +0530

    deployment requirements

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s_ud7uf7^$+52_2%uq(6k-+#2mtc2p=%udo2!ho)@nug67el08'
+SECRET_KEY =os.environ.get('DJANGO_SECRET_KEY','s_ud7uf7^$+52_2%uq(6k-+#2mtc2p=%udo2!ho)@nug67el08') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG','') =='True'
 
 ALLOWED_HOSTS = []
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -76,12 +77,18 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#If its in production then prosgresql else sqllite
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+    'default': dj_database_url.config(conn_max_age=500)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -121,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=BASE_DIR / 'staticfiles'
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = "/book"
---
+++ deploy.md
@@ -0,0 +1,24 @@
+python manage.py check --deploy
+
+secret key
+
+debug
+
+database pip install dj-database-url 
+pip install psycopg2-binary
+
+ import dj_database_url 
+ dj_database_url.config(conn_max_age=500)
+
+serving static files 
+static root 
+pip install whitenoise 
+'whitenoise.middleware.WhiteNoiseMiddleware',
+
+HTTP Server pip install gunicorn
+
+export requirements
+
+For Heroku procfile web: gunicorn projectName.wsgi --log-file -
+
+Create Heroku app and Add Allowed hosts
